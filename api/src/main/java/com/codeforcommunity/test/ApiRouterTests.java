29359c1f818e08848c3d2f673bc986ad7c225ebd
Brandon Liang <54skyxenon@gmail.com>
2020-01-28 12:44:17 -0500

    Trying to add tests to routes

---
+++ api/src/main/java/com/codeforcommunity/test/ApiRouterTests.java
@@ -0,0 +1,9 @@
+package com.codeforcommunity.test;
+
+import com.codeforcommunity.rest.ApiRouter;
+import io.vertx.core.Vertx;
+//import io.vertx.ext.unit.TestSuite;
+
+public class ApiRouterTests {
+
+}
