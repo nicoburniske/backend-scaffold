3928f84fe8249fdb9cdc665b76627d076624d85a
Brandon Liang <54skyxenon@gmail.com>
2020-02-01 16:17:03 -0500

    Reorganized testing directory

--- api/src/test/java/com/codeforcommunity/rest/ApiRouterTests.java
+++ api/src/test/java/com/codeforcommunity/rest/ApiRouterTests.java
@@ -2,7 +2,13 @@ package com.codeforcommunity.test;
 
 import com.codeforcommunity.rest.ApiRouter;
 import io.vertx.core.Vertx;
-//import io.vertx.ext.unit.TestSuite;
+import io.vertx.ext.unit.TestSuite;
+
+/*
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mockito;
+*/
 
 public class ApiRouterTests {
 
