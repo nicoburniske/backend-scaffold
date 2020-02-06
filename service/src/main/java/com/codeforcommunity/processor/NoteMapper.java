978684cfdae87e5d3d517e96289277961d408cc6
Aaron Pradhan <apradhan@tripadvisor.com>
2020-02-05 20:01:24 -0500

    Note mapper and tests

--- service/src/main/java/com/codeforcommunity/processor/NoteMapper.java
+++ service/src/main/java/com/codeforcommunity/processor/NoteMapper.java
@@ -1,4 +1,16 @@
 package com.codeforcommunity.processor;
 
+import com.codeforcommunity.dto.notes.ContentNote;
+import org.jooq.generated.tables.records.NoteRecord;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
 public interface NoteMapper {
+
+    NoteMapper INSTANCE = Mappers.getMapper(NoteMapper.class);
+
+    @Mapping(source = "content", target = "body")
+    NoteRecord contentNoteToNoteRecord(ContentNote contentNote);
 }
--- service/src/test/java/com.codeforcommunity/processor/NoteMapperTest.java
+++ service/src/test/java/com.codeforcommunity/processor/NoteMapperTest.java
@@ -1,5 +1,19 @@
+package com.codeforcommunity.processor;
+
+import com.codeforcommunity.dto.notes.ContentNote;
+import org.jooq.generated.tables.records.NoteRecord;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class NoteMapperTest {
+public class NoteMapperTest {
 
+    @Test
+    public void contentNoteToNoteRecord() {
+        ContentNote contentNote = new ContentNote("title", "content");
+        NoteRecord noteRecord = NoteMapper.INSTANCE.contentNoteToNoteRecord(contentNote);
+        assertNotNull(noteRecord);
+        assertEquals("content", noteRecord.getBody());
+        assertEquals("title", noteRecord.getTitle());
+    }
 }
